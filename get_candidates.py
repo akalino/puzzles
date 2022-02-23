5a34c2de6a451916fdc12c6c172ee26e82afd2fe
Alex Kalinowski <kalinowski.aj@gmail.com>
2022-02-23 14:19:58 -0500

    Add files via upload

--- get_candidates.py
+++ get_candidates.py
@@ -2,6 +2,8 @@ import itertools
 import nltk
 import sys
 
+from wordfreq import zipf_frequency
+
 
 def all_words():
      _vocab = set(w.lower() for w in nltk.corpus.words.words())
@@ -9,7 +11,7 @@ def all_words():
 
 
 def search_space(_beehive, _vocab):
-    test_lengths = [4,5,6,7,8,9]
+    test_lengths = [4, 5, 6, 7, 8, 9]
     if len(_beehive) != 7:
         print('Not enough letters!')
         sys.exit()
@@ -21,10 +23,13 @@ def search_space(_beehive, _vocab):
             for k in cands:
                 if center_letter in k:
                     if ''.join(list(k)) in _vocab:
-                        print(''.join(list(k)))
+                        _w = ''.join(list(k))
+                        score = zipf_frequency(_w, 'en')
+                        if score > 0.0:
+                            print('{}: {}'.format(_w, score))
 
 
 if __name__ == "__main__":
-    beehive = ['h', 'l', 'i', 'c', 'd', 'e', 'k']
+    beehive = ['e', 'j', 'o', 'c', 't', 'b', 'd']
     vocab = all_words()
     search_space(beehive, vocab)
